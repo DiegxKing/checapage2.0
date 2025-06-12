7656e4795676403dbfaef382f1971d62bd553a79
DiegxKing <jazaneropq1@upao.edu.pe>
2025-06-12 01:50:39 -0500

    coomit

--- backend/db_mysql.py
+++ backend/db_mysql.py
@@ -1,15 +1,20 @@
 # backend/db_mysql.py
+import os
 import mysql.connector
 from datetime import datetime
+from dotenv import load_dotenv
+
+
+load_dotenv()
 
 def guardar_deteccion_mysql(url, resultado, probabilidad, tiempo_ms):
     try:
         conn = mysql.connector.connect(
-            host="mainline.proxy.rlwy.net",
-            user="root",
-            password="msBquYSEmjeIoGWnSVldESZRakntFwrK",
-            database="railway",
-            port=38433
+            host=os.getenv("MYSQL_HOST"),
+            port=int(os.getenv("MYSQL_PORT")),
+            user=os.getenv("MYSQL_USER"),
+            password=os.getenv("MYSQL_PASSWORD"),
+            database=os.getenv("MYSQL_DB")
         )
 
         cursor = conn.cursor()
